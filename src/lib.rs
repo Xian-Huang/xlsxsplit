cfcdd41af82d6a881be3c7e380f5e395753cdd5b
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-02-02 21:53:23 +0800

    split main

--- src/lib.rs
+++ src/lib.rs
@@ -89,3 +89,13 @@ pub fn check_path_exist(path:&str)->&Path{
     }
     cpath
 }
+
+pub fn split_main(){
+    let header_number = 2u32; //header 行数 前几行
+    let col_index = 1;
+    let output = "./data";
+    let book = &read_xlsx("./test.xlsx"); //读取文件
+    let sheet = book.get_sheet_collection().first().unwrap(); //获取第一个sheet
+    let path = check_path_exist(output);
+    split_book(sheet, header_number,col_index,path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 /*
 author:lidingyi
 */
-use xlsxsplit::{read_xlsx,split_book,check_path_exist};
+// use xlsxsplit::{read_xlsx,split_book,check_path_exist,split_main};
 
 fn main() {
-    let header_number = 2u32; //header 行数 前几行
-    let col_index = 1;
-    let output = "./data";
-    let book = &read_xlsx("./test.xlsx"); //读取文件
-    let sheet = book.get_sheet_collection().first().unwrap(); //获取第一个sheet
-
-    let path = check_path_exist(output);
-    split_book(sheet, header_number,col_index,path);
+    
 }
