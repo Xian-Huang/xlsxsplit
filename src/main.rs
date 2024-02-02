558809e992ffd3af2c51401ebee23f128e4649a7
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-02-02 22:22:15 +0800

    添加slint 窗体

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 author:lidingyi
 */
 // use xlsxsplit::{read_xlsx,split_book,check_path_exist,split_main};
+slint::include_modules!();
 
 fn main() {
-    
+    MainWindow::new().unwrap().run().unwrap();
 }
