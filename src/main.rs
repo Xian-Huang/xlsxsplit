8cc667795dcb9f9f9452d858256a9a11edb8fd09
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-02-06 10:37:22 +0800

    多个weak 后面要改

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ fn main() {
     //创建主页面
     let mainwidow: MainWindow = MainWindow::new().expect("MainWindow create fail!");
     let weak = mainwidow.as_weak().unwrap();
-    mainwidow.on_selectfile( || select_file(&weak));
-    mainwidow.on_selectpath( || select_path(&weak));
+    mainwidow.on_selectfile(move || select_file(&weak));
+    let weak = mainwidow.as_weak().unwrap();
+    mainwidow.on_selectpath(move || select_path(&weak));
 
     //装载分离功能
     presplitbook(&mainwidow);
