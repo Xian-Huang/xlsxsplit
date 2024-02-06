7dcfaf42dbf95fc1cc3cd3ea2464aa32edd5a8ca
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-02-06 09:29:00 +0800

    文本修改

--- src/lib.rs
+++ src/lib.rs
@@ -125,23 +125,21 @@ pub fn select_file()->SharedString {
             println!("{:?}",files.to_str().unwrap());
             return files.to_str().unwrap().to_string().into();
     }else {
-        println!("选择目录失败");
+        println!("选择输入文件失败");
         return SharedString::new();
     }
 }
 
 
-//选择文件
+//选择输出目录
 pub fn select_path()->SharedString {
     let files = FileDialog::new()
-        .add_filter("dir", &[""])
-        .set_directory("/")
-        .pick_file();
+        .pick_folder();
     if let Some(files) = files{
             println!("{:?}",files.to_str().unwrap());
             return files.to_str().unwrap().to_string().into();
     }else {
-        println!("选择目录失败");
+        println!("选择输出目录失败");
         return SharedString::new();
     }
 }
